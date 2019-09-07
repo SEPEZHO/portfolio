5c135f613770a155715cea7c98e763e6553827ca
Sepezho <koko2kocit@gmail.com>
2019-09-07 19:43:58 +0300

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header_mp4 from '../static/Header_mp4.mp4';
+import Title_header from './Title_header.jsx';
 
 const Header = () => {
   return (
     <div className='header' >
     	<video src={Header_mp4} type='video/mp4' autoPlay loop muted/>
-    	Header
+    	<Title_header />
     </div>
   );
 }
---
+++ src/components/Title_header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Title_header = () => {
+  return (
+   	<div className='Title_header'>
+  		<div className='Title_header_name'>
+   			Created by: Vladislav Bliznyuk.
+  		</div>
+  		<div className='Title_header_subs'>
+  			YouTube: 0
+  		</div>
+  	</div>
+  );
+}
+
+export default Title_header;
