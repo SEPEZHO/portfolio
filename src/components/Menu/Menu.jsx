9ec4ae7cf22473f58e6df5593670f7b3b035c1b3
sepezho <koko2kocit@gmail.com>
2019-09-28 20:59:26 +0300

    doing menu.

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import s from './Menu.module.css';
 
-const Menu = () => {
-  return (
-    <div className ={s.Menu}>
-    	<div className={s.Contact}><div></div><a href='/Contact'>Contact</a></div>
-    	<div className={s.Main}><div></div><a href='/Main'>Main</a></div>
-    	<div className={s.Projects}><div></div><a href='/Projects'>Projects</a></div>
-    </div>
-  );
-}
+class QuestionListItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      background: 'none',
+    };
+  }
 
-export default Menu;
+  componentDidMount = () => {
+    window.addEventListener('scroll', () => {
+    	if(window.scrollY){
+      	 this.setState({
+      		background: 'white',
+     	 })
+  		}else{
+  			this.setState({
+      		background: 'none',
+     	 })
+  		}
+    });
+  };
+
+  render() {
+    return (
+	    <div style={{ background: this.state.background }} className ={s.Menu}>
+    		<div className={s.Contact}><div></div><a href='/Contact'>Contact</a></div>
+    		<div className={s.Main}><div></div><a href='/Main'>Main</a></div>
+    		<div className={s.Projects}><div></div><a href='/Projects'>Projects</a></div>
+    	</div>
+    );
+  }
+}
+export default QuestionListItem;
