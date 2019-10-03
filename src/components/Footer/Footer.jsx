bb4cb9494170d5e2265e5556151d93e763d11003
Sepezho <koko2kocit@gmail.com>
2019-10-03 22:21:04 +0300

    Добавил кнопку на верх и занялся футером

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,10 @@ import s from './Footer.module.css';
 const Footer = () => {
   return (
     <div className={s.Footer}>
-    	Footer
+    	<div className={s.Menu}>
+    	<div className={s.Main}><a href='/'>Main</a></div>
+    	<div className={s.Contact}><a href='/Contact'>Contact</a></div>
+    </div>
     </div>
   );
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,36 +1,48 @@
 import React from 'react';
+import ToTop from '../../Static/Images/ToTop.png';
+
 import s from './Menu.module.css';
 
-class QuestionListItem extends React.Component {
+class Menu extends React.Component {
   constructor() {
     super();
     this.state = {
       background: 'none',
+      display: 'none'
+
     };
   }
 
   componentDidMount = () => {
     window.addEventListener('scroll', () => {
-    	if(window.scrollY){
+    	if(window.scrollY > (document.documentElement.clientHeight)*0.2){
+        console.log();
       	 this.setState({
       		background: 'white',
+          display: 'block'
      	 })
   		}else{
   			this.setState({
       		background: 'none',
+          display: 'none'
      	 })
   		}
     });
   };
-
+  ToTop = () =>{
+    window.scrollTo(window.scrollTo, 0);
+  }
   render() {
     return (
-	    <div style={{ background: this.state.background }} className ={s.Menu}>
-    		<div className={s.Contact}><div></div><a href='/Contact'>Contact</a></div>
-    		<div className={s.Main}><div></div><a href='/Main'>Main</a></div>
-    		<div className={s.Projects}><div></div><a href='/Projects'>Projects</a></div>
+      <div>
+	      <div style={{ background: this.state.background }} className ={s.Menu}>
+    	 	  <div className={s.Contact}><div></div><a href='/Contact'>Contact</a></div>
+    		  <div className={s.Main}><div></div><a href='/'>Main</a></div>
+    		  <div className={s.Projects}><div></div><a href='/Projects'>Projects</a></div>
+        </div>
+        <img alt='' src={ToTop} className={s.ToTop} onClick={this.ToTop} style={{ display: this.state.display }} />
     	</div>
     );
   }
 }
-export default QuestionListItem;
+export default Menu;
