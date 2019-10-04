341c41676dfc4699b1462b49beae2697c714faff
Sepezho <koko2kocit@gmail.com>
2019-10-04 20:55:57 +0300

    Делаю пост запрос при нажатии на like

---
+++ src/Likes/Response.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const port = 3001
+const pool = require('../GitHubApi/MysqlCon.js').pool;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/Likes', function(req, res) {
+
+    pool.getConnection((err, con) => {
+        if (err) console.log('Err: ' + err); // not connected!
+
+        // Use the connection
+        con.query('SELECT * FROM `info`', (error, results) => {
+            if (error) console.log('Err: ' + error); // not connected!
+            con.release();
+            res.send(results);
+        });
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log('\n-------------------------------------------');
+    console.log(`API server is listening on ${port}`)
+    console.log('-------------------------------------------\n');
+})
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,15 +2,33 @@ import React from 'react';
 
 import s from './Footer.module.css';
 
-const Footer = () => {
+class Footer extends React.Component {
+  
+   constructor() {
+    super();
+    this.state = {
+    	Likes: 0
+    };
+  }
+
+  Like = () =>{
+  	fetch('/Likes', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            }
+        })
+  }
+render(){
   return (
     <div className={s.Footer}>
     	<div className={s.Menu}>
-    	<div className={s.Main}><a href='/'>Main</a></div>
-    	<div className={s.Contact}><a href='/Contact'>Contact</a></div>
+    		<div className={s.Main}><a href='/'>Main</a></div>
+    		<div className={s.Contact}><a href='/Contact'>Contact</a></div>
+    	</div>
+    	<div className={s.LikeBtn} onClick={this.Like}><span>LikeBtn</span></div>
     </div>
-    </div>
-  );
+  )}
 }
 
 export default Footer;
