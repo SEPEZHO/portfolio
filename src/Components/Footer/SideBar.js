a4d0b58b32a5975b803565b6a58234ea7d9862b4
sepezho <sepezho@gmail.com>
2019-10-14 13:56:23 +0300

    Попытался сделать sideBar в header.

--- src/Components/Footer/SideBar.js
+++ src/Components/Footer/SideBar.js
@@ -10,10 +10,10 @@ import SrcVk from '../../Static/Images/Icons/Vk.png'
 const SideBar = () => {
     return (
         <div className={s.SideBar}>
-        	<a href='https://stackoverflow.com/users/12073046/sepezho'><img className={s.SrcGithub} src={SrcGithub} alt=''/></a>
-        	<a href='https://github.com/SEPEZHO'><img className={s.SrcStackOverflow} src={SrcStackOverflow} alt=''/></a>
-        	<a href='https://www.youtube.com/channel/UC4nIyTlfA6DKuF_xKtefutQ/featured?view_as=subscriber'><img className={s.SrcYouTube} src={SrcYouTube} alt=''/></a>
-        	<a href='https://vk.com/vlad_ascetic'><img className={s.SrcVk} src={SrcVk} alt=''/></a>
+        	<div><a href='https://github.com/SEPEZHO'><img src={SrcGithub} alt=''/></a></div>
+        	<div><a href='https://stackoverflow.com/users/12073046/sepezho'><img src={SrcStackOverflow} alt=''/></a></div>
+        	<div><a href='https://www.youtube.com/channel/UC4nIyTlfA6DKuF_xKtefutQ/featured?view_as=subscriber'><img src={SrcYouTube} alt=''/></a></div>
+        	<div><a href='https://vk.com/vlad_ascetic'><img src={SrcVk} alt=''/></a></div>
         </div>
     )
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderImg from '../../Static/Images/Me.jpg';
 import TitleHeader from './TitleHeader.jsx';
+import SideBar from '../Footer/SideBar.js';
 
 import s from './Header.module.css';
 
@@ -8,6 +9,7 @@ const Header = () => {
   return (
     <div className={s.Header}>
     	<img alt='' src={HeaderImg}/>
+    	<SideBar />
     	<TitleHeader />
     </div>
   );
