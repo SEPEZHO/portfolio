3952ce1c31acde49fa00f79e425976d364f839bb
sepezho <sepezho@gmail.com>
2019-10-18 22:08:54 +0300

    Поправил sidebar в header-е

--- src/Components/MainPage/Header/Header.jsx
+++ src/Components/MainPage/Header/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import HeaderImg from '../../../Static/Images/Me.jpg';
 import TitleHeader from './TitleHeader.jsx';
-import SideBar from '../../Footer/SideBar.jsx';
 
 import s from './Header.module.css';
 
 const Header = () => {
   return (
     <div className={s.Header}>
-    	<SideBar />
     	<img alt='' src={HeaderImg}/>
     	<TitleHeader />
     </div>
--- src/Components/MainPage/MainPageRender.jsx
+++ src/Components/MainPage/MainPageRender.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import SideBar from '../Footer/SideBar.jsx';
 import Header from './Header/Header.jsx';
 import Main from './Main/Main.jsx';
 
 const MainPageRender = () => {
   return (
-    <div>
+    <div >
+    	<SideBar />
     	<Header />
     	<Main />
     </div>
