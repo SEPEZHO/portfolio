6af2df0f175a6ba0ae1baf613fb0740f8c3887c9
sepezho <sepezho@gmail.com>
2019-11-25 21:42:28 +0300

    Сделал нормальный бок Лучшие Проекты. Поменял чутка стили для About.

--- src/Components/MainPage/Main/MainProjects.jsx
+++ src/Components/MainPage/Main/MainProjects.jsx
@@ -11,38 +11,64 @@ const MainProjects = () => {
 
     return (
         <div className={s.Projects}>
-        <h1>Лучшие проекты</h1>
+          <h1>Лучшие проекты</h1>
         
-        <div className={s.Proj}>
-      <a href='https://github.com/SEPEZHO/Portfolio'>
-      <span className={s.Text} style={{'filter': 'invert(1)'}}>Портфолио</span>
-      <span className={s.TextDesc} style={{'filter': 'invert(1)'}}>Это сайт, на котором вы сейчас находитесь. Использовал: react + node js + nginx.</span>
-        <img alt='' src={Portfolio} className={s.ProjImg} />
-</a>    
-    </div>
+          <div className={s.Proj}>
+            <div className={s.Text}>
+              Это сайт, на котором вы сейчас находитесь.<br /> Использовал: react + node js + nginx.
+            </div>
+            <a href='https://github.com/SEPEZHO/Portfolio'>
+              <div className={s.Image}>
+                <h2>
+                  Портфолио
+                </h2>
+                <img alt='' src={Portfolio} className={s.ProjImg} />
+              </div>
+            </a>
+          </div>
+          
+          <div className={s.Proj1}>
+            <div className={s.Text1}>
+              Игра кликер, с системой регистрации, входа, и ,соответственно, сохранением игровых данных на сервере. Имеется чат, построенный на защищенных вебсокетах (wss).<br /> Использовал: jQuery + php + mysql.
+            </div>
+            <a href='https://github.com/SEPEZHO/Clicker'>
+              <div className={s.Image1}>
+                <h2>
+                  Кликер
+                </h2>
+                <img alt='' src={Clicker} className={s.ProjImg} />
+              </div>
+            </a>
+          </div>
 
-    <div className={s.Proj1}>
-      <a href='https://github.com/SEPEZHO/Clicker'>
-      <span className={s.Text}>Кликер</span>
-      <span className={s.TextDesc}>Игра кликер, с системой регистрации, входа, и ,соответственно, сохранением игровых данных на сервере. Имеется чат, построенный на защищенных вебсокетах (wss). Использовал: jQuery + php + mysql.</span>
-        <img alt='' src={Clicker} className={s.ProjImg} />
-</a>    
-    </div>
+          <div className={s.Proj}>
+            <div className={s.Text}>
+              Стандартная игра жизнь, базируется на canvas.<br />Использовал: jQuery.
+            </div>
+            <a href='https://github.com/SEPEZHO/Game-OF-life'>
+              <div className={s.Image}>
+                <h2>
+                  Игра жизнь
+                </h2>
+                <img alt='' src={Life} className={s.ProjImg} />
+              </div>
+            </a>
+          </div>
+          
+          <div className={s.Proj1}>
+            <div className={s.Text1}>
+              Самописный алгоритм нахождения пути от одной точки до другой, на canvas.<br /> Использовал: jQuery.
+            </div>
+            <a href='https://github.com/SEPEZHO/ShortWay'>
+              <div className={s.Image1}>
+                <h2>
+                  Путь
+                </h2>
+                <img alt='' src={ShortWay} className={s.ProjImg} />
+              </div>
+            </a>
+          </div>
 
-<div className={s.Proj2}>
-      <a href='https://github.com/SEPEZHO/Game-OF-life'>
-      <span className={s.Text} style={{'filter': 'invert(1)'}}>Игра жизнь</span>
-      <span className={s.TextDesc} style={{'filter': 'invert(1)'}}>Стандартная игра жизнь, базируется на canvas. Использовал: jQuery.</span>
-        <img alt='' src={Life} className={s.ProjImg}/>
-</a>    
-    </div>
-    <div className={s.Proj3}>
-      <a href='https://github.com/SEPEZHO/ShortWay'>
-      <span className={s.Text}>Путь</span>
-      <span className={s.TextDesc}>Самописный алгоритм нахождения пути от одной точки до другой, на canvas. Использовал: jQuery.</span>
-        <img alt='' src={ShortWay} className={s.ProjImg}/>
-</a>    
-    </div>
     </div>
     )
 }
