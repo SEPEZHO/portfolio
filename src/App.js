0ed27789cf67913139ae9efd950ce1e73caa9cce
sepezho <sepezho@gmail.com>
2019-12-01 17:41:18 +0300

    Сделал балдежную анимацию при загрузке страницы.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
     };
   }
   pathMain = val => {
+    document.documentElement.scrollTop = 0;
     this.setState({ path: val });
   };
   render() {
--- src/Components/RenderBody.jsx
+++ src/Components/RenderBody.jsx
@@ -4,7 +4,17 @@ import MainPageRender from "./MainPage/MainPageRender.jsx";
 import ContactPageRender from "./ContactPage/ContactPageRender.jsx";
 import Projects from "./Projects/Projects.jsx";
 
+import loadGif from "../Static/Images/Loading.gif";
+import s from "./style.module.sass";
+
 class Body extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      styleLoad: {
+      }
+    };
+  }
   componentDidMount() {
     fetch("https://sepezho.ru:7777/API/RepCom", {
       method: "POST"
@@ -20,28 +30,49 @@ class Body extends React.Component {
             dataCom: data.Com.slice(0, 11)
           }
         })
+        this.loading()
       })
       .catch(error => {
         console.log(error);
       });
+    
+    }
+
+loading(){
+  if(this.state.dataRepCom){
+      setTimeout(()=>{
+        document.documentElement.scrollTop = 0;
+        this.setState({ styleLoad:{
+          opacity: 0,
+          visibility: 'hidden'
+        }});
+       }, 1000)
   }
+}
 
   renderMain() {
-    document.documentElement.scrollTop = 0;
-
-    if (this.props.path === "/Main" && this.state) {
-      return <MainPageRender pathMain={this.props.pathMain} dataCom={this.state.dataRepCom.dataCom.slice(0, 6)}/>
-    } else if (this.props.path === "/Projects" && this.state) {
-      return <Projects dataRepCom={this.state.dataRepCom}/>;
-    } else if (this.props.path === "/Contact" && this.state) {
-      return <ContactPageRender/>;
-    } else if (this.state){
-      return <MainPageRender pathMain={this.props.pathMain}/>;
+    if(this.state.dataRepCom){
+      if (this.props.path === "/Main") {
+        return <MainPageRender pathMain={this.props.pathMain} dataCom={this.state.dataRepCom.dataCom.slice(0, 6)}/>
+      } else if (this.props.path === "/Projects") {
+        return <Projects dataRepCom={this.state.dataRepCom}/>;
+      } else if (this.props.path === "/Contact") {
+        return <ContactPageRender/>;
+      } else {
+        return <MainPageRender pathMain={this.props.pathMain}/>;
+      }
     }
   };
 
   render() {
-    return <div>{this.renderMain()}</div>
+    return (<div>
+    <div style={this.state.styleLoad} className={s.Loading}>
+      <img alt='' src={loadGif}/>
+    </div>
+
+    {this.renderMain()}
+    </div>
+    )
   }
 }
 
