ddb89e068e881b9008ee44e24137e5cd058695ca
sepezho <sepezho@gmail.com>
2019-12-22 20:19:22 +0300

    Правки в написанный текст. Пару правок по стилям.

--- src/Components/Footer/Text.jsx
+++ src/Components/Footer/Text.jsx
@@ -5,12 +5,12 @@ import s from "./Footer.module.sass";
 const Text = () => {
   return (
     <div className={s.Text}>
-      <h1>Создано sepezho</h1>
+      <h1>Создан sepezho</h1>
       <div>
         Время создания:
         <nav>
           <li>С: 1.09.2019</li>
-          <li>До: __.__.____</li>
+          <li>До: 23.12.2019</li>
         </nav>
         <br />
         <br />
